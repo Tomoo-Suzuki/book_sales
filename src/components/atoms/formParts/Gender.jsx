19793af545041724284c602a9ba3ee8adcec6951
Tomoo Suzuki <solosoprani2002@gmail.com>
2020-04-19 15:44:32 +0900

    #21 form update3

--- src/components/atoms/formParts/Gender.jsx
+++ src/components/atoms/formParts/Gender.jsx
@@ -6,18 +6,36 @@ const Gender = (props) => {
   return (
     <dl>
       <dt>ジェンダー</dt>
-      <dd>
+      <dd className="radioAndCheckbox">
         <label>
           <span>男性</span>
-          <input id="type" type="radio" name="gender" />
+          <input
+            type="radio"
+            name="gender"
+            value="man"
+            checked={val.gender === "man"}
+            onChange={(e) => updateState(e)}
+          />
         </label>
         <label>
           <span>女性</span>
-          <input id="type" type="radio" name="gender" />
+          <input
+            type="radio"
+            name="gender"
+            value="woman"
+            checked={val.gender === "man"}
+            onChange={(e) => updateState(e)}
+          />
         </label>
         <label>
           <span>その他</span>
-          <input id="type" type="radio" name="gender" />
+          <input
+            type="radio"
+            name="gender"
+            value="lgbt"
+            checked={val.gender === "lgbt"}
+            onChange={(e) => updateState(e)}
+          />
         </label>
       </dd>
     </dl>
--- src/lib/setFormData.js
+++ src/lib/setFormData.js
@@ -1,4 +1,6 @@
 export default function setFormData(e) {
+  console.log(e.target.name)
+  console.log(e.target.value)
   this.setState({
     [e.target.name]: e.target.value
   });
