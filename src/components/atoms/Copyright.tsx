72d8660f778f79275d5604243dc79ddb80120798
Tomoo Suzuki <solosoprani2002@gmail.com>
2023-09-28 22:02:08 +0900

    update

--- src/components/atoms/Copyright.tsx
+++ src/components/atoms/Copyright.tsx
@@ -2,5 +2,5 @@ import * as React from "react";
 import "_scss/atoms/_copyright";
 
 export default function Copyright(): JSX.Element {
-  return <div className="copyright">©1996-2020, BookMa.com</div>;
+  return <div className="copyright">©1996, BookMa.com</div>;
 }
